14d4c48c2832d359b12af4c2f2aada1f43d76ace
Subham.XD <subham.panda.dev@gmail.com>
2026-09-10 07:41:16 +0530

    Update main.py

--- main.py
+++ main.py
@@ -12,8 +12,11 @@ bot = commands.Bot(command_prefix="!", intents=intents)
 async def on_ready():
     print(f'Logged on as {bot.user}!')
 
+def Emoji(name):
+    return f'discord.utils.get(bot.emojis, name="name")
+
 @bot.command()
 async def balance(ctx):
-    await ctx.send(f"{ctx.author.name}, you have 0 coins.")
+    await ctx.send(f"{ctx.author.name}, you have 0 coins. (Emoji(godofspaceavatar))")
 
 bot.run(os.environ["APP_TOKEN"])
