2c53e2b4990da7da6b1b9e8a26cca0762d76cfab
Subham.XD <subham.panda.dev@gmail.com>
2026-09-10 08:28:24 +0530

    Update main.py

--- main.py
+++ main.py
@@ -11,10 +11,10 @@ bot = commands.Bot(command_prefix="!", intents=intents)
 @bot.event
 async def on_ready():
     print(f'Logged on as {bot.user}!')
-    await bot.fetch_application_emojis()
+    bot.app_emojis = await bot.fetch_application_emojis()
 
 def emoji(name):
-    return discord.utils.get(bot.application_emojis, name=name)
+    return discord.utils.get(bot.app_emojis, name=name)
 
 @bot.command()
 async def balance(ctx):
