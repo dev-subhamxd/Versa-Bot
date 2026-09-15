c2b0c4c23b60e9bb7660e2d158ed70cd9f6435fc
Subham.XD <subham.panda.dev@gmail.com>
2026-09-15 18:29:56 +0530

    Integrate aiohttp and create custom bot class
    Added aiohttp for HTTP requests and implemented a custom bot class to manage the session.

--- main.py
+++ main.py
@@ -3,11 +3,21 @@ import os
 import discord
 from discord.ext import commands
 
+import aiohttp
+
 intents = discord.Intents.default()
 intents.message_content = True
 
 bot = commands.Bot(command_prefix="!", intents=intents)
-    
+
+class MyBot(commands.Bot):
+    async def setup_hook(self):
+        self.session = aiohttp.ClientSession()
+
+    async def close(self):
+        await self.session.close()
+        await super().close()
+
 @bot.event
 async def on_ready():
     print(f'Logged on as {bot.user}!')
